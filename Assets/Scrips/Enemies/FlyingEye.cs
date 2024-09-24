6772dfd5a5ba7f434d15cee6f795eb57308f651c
Axi-Alfred <alfred.pereira.f@gmail.com>
2024-09-25 00:04:33 +0200

    Testade lite, inget nytt än

--- Assets/Scrips/Enemies/FlyingEye.cs
+++ Assets/Scrips/Enemies/FlyingEye.cs
@@ -53,4 +53,5 @@ public class FlyingEye : MonoBehaviour
     }
 
 
+
 }
--- Assets/Scrips/Health.cs
+++ Assets/Scrips/Health.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Health : MonoBehaviour
 {
     [SerializeField] private int health = 100;
