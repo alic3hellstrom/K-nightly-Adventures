992a5680c48800893ea477ad9284fefdd1073bda
Fulip <98706116+DaFxqx19@users.noreply.github.com>
2024-09-27 20:07:10 +0200

    Revert "Merge pull request #6 from alic3hellstrom/ALICEFILIPBRANCHOFFTOALFRED"
    This reverts commit 39fa22a65b7e74489737ac4dec69c2b03c66663e, reversing
    changes made to 16e4bb920aab8b0176468e4fc4f573984e9007ea.

--- Assets/Scrips/AttackArea.cs
+++ Assets/Scrips/AttackArea.cs
@@ -1,61 +1,17 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Runtime.CompilerServices;
-using Unity.Mathematics;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 public class AttackArea : MonoBehaviour
-
 {
-    [SerializeField] private GameObject player;
+    private int damage = 10;
 
-    [SerializeField] private int damage = 10;
-    //private int damage = 10;
-
-    private void OnTriggerStay2D(Collider2D other)
+    private void OnTriggerEnter2D(Collider2D collider)
     {
-        Console.Clear();
-
-        //if (collider.GetComponent<Health>() != null)
-        //{
-        //    float xAxis = collider.transform.position.x - playerTrans.position.x;
-
-        //    print(-xAxis + ", " + 0);
-        //    collider.GetComponent<Rigidbody2D>().AddForce(new Vector2(xAxis * 40, 0));
-        //    Health health = collider.GetComponent<Health>();
-        //    health.Damage(damage);
-        //}
-        //print(player.GetComponent<PlayerAttack>().attacking);
-        //print("Tried attacking");
-        //if (player.GetComponent<PlayerAttack>().attacking == true)
-        //{
-        //    print("Detected attacking");
-        //}
-        //if (other.CompareTag("Enemy"))
-        //{
-        //    print("Detected enemy as enemy");
-        //    print("Their name = " + other.name);
-        //    print(other.name);
-        //}
-        PlayerMovement pM = player.GetComponent<PlayerMovement>();
-        Health hp = other.GetComponent<Health>();
-        if (player.GetComponent<PlayerAttack>().attacking && other.CompareTag("Enemy") && pM.CheckIfGrounded())
+        if(collider.GetComponent<Health>() != null)
         {
-            print("Attacking Enemy" + other.name);
-
-            if (other.transform.position.x > player.transform.position.x && pM.lookingRight)
-            {
-                other.GetComponent<Rigidbody2D>().AddForce(new(2f, 1.5f), ForceMode2D.Impulse);
-                hp.Damage(damage);
-            }
-            else if (other.transform.position.x < player.transform.position.x && !pM.lookingRight)
-            {
-                other.GetComponent<Rigidbody2D>().AddForce(new(-2f, 1.5f), ForceMode2D.Impulse);
-                hp.Damage(damage);
-            }
+            Health health = collider.GetComponent<Health>();
+            health.Damage(damage);
         }
     }
 }
--- Assets/Scrips/PlayerMovement.cs
+++ Assets/Scrips/PlayerMovement.cs
@@ -14,8 +14,6 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private Transform spawnPosition;
 
     public float Health;
-    public bool lookingRight = true;
-
     private bool isGrounded;
     private float rayDistance = 0.25f;
     private float horizontalValue;
@@ -24,7 +22,7 @@ public class PlayerMovement : MonoBehaviour
     private Animator anim;
 
     // Start is called before the first frame update
-    private void Start()
+    void Start()
     {
         healthBar.value = Health;
         rgbd = GetComponent<Rigidbody2D>();
@@ -33,25 +31,23 @@ public class PlayerMovement : MonoBehaviour
     }
 
     // Update is called once per frame
-    private void Update()
+    void Update()
     {
         horizontalValue = Input.GetAxis("Horizontal");
 
-        if (horizontalValue < 0)
+       if(horizontalValue < 0)
         {
             FlipSprite(true);
-            lookingRight = false;
         }
 
         if (horizontalValue > 0)
         {
             FlipSprite(false);
-            lookingRight = true;
         }
 
         CheckIfGrounded();
 
-        if (Input.GetButtonDown("Jump") && CheckIfGrounded() == true)
+        if(Input.GetButtonDown("Jump") && CheckIfGrounded() == true)
         {
             Jump();
         }
@@ -59,9 +55,11 @@ public class PlayerMovement : MonoBehaviour
         anim.SetFloat("MoveSpeed", Mathf.Abs(rgbd.velocity.x));
         anim.SetFloat("VerticalSpeed", rgbd.velocity.y);
         anim.SetBool("IsGrounded", CheckIfGrounded());
+
+        
     }
 
-    private void FixedUpdate()
+    void FixedUpdate()
     {
         rgbd.velocity = new Vector2(horizontalValue * moveSpeed * Time.deltaTime, rgbd.velocity.y);
     }
@@ -75,8 +73,7 @@ public class PlayerMovement : MonoBehaviour
     {
         rgbd.AddForce(new Vector2(0, jumpForce));
     }
-
-    public bool CheckIfGrounded()
+    private bool CheckIfGrounded()
     {
         RaycastHit2D leftHit = Physics2D.Raycast(leftFoot.position, Vector2.down, rayDistance, whatIsGround);
         RaycastHit2D rightHit = Physics2D.Raycast(rightFoot.position, Vector2.down, rayDistance, whatIsGround);
@@ -93,17 +90,19 @@ public class PlayerMovement : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.layer.Equals(8)) // 8 stï¿½r fï¿½r layer 8 som ï¿½r enemies, om spelaren krockar med enemies, fï¿½r den 10 i skada.
-        {
+        if(collision.gameObject.layer.Equals(8)) // 8 står för layer 8 som är enemies, om spelaren krockar med enemies, får den 10 i skada. 
+            {
             //Health = Health - 10;
             Health -= 10;
             healthBar.value = Health;
         }
     }
-
     private void Respawn()
     {
+
         transform.position = spawnPosition.position;
         rgbd.velocity = Vector2.zero;
     }
+
+
 }
