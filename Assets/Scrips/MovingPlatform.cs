4a63334dd08ac042d7d8b5d7d52435d4b620d930
Axi-Alfred <alfred.pereira.f@gmail.com>
2024-10-02 01:55:47 +0200

    Scene2

---
+++ Assets/Scrips/MovingPlatform.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    [SerializeField] private Transform target1, target2;
+    [SerializeField] private float moveSpeed = 2.0f;
+
+    private Transform currentTarget;
+    void Start()
+    {
+        currentTarget = target1;
+    }
+
+    void fixedUpdate()
+    {
+        if (transform.position == target1.position)
+        {
+            currentTarget = target2;
+        }
+
+        if (transform.position == target2.position)
+        {
+            currentTarget = target1;
+        }
+
+        transform.position = Vector2.MoveTowards(transform.position, currentTarget.position, moveSpeed * Time.deltaTime);
+    }
+}
