7f04e678feb7358ef790320fd9540bc6007a0e7a
Axi-Alfred <alfred.pereira.f@gmail.com>
2024-10-02 02:26:23 +0200

    Scene 2

--- Assets/Scrips/MovingPlatform.cs
+++ Assets/Scrips/MovingPlatform.cs
@@ -8,23 +8,42 @@ public class MovingPlatform : MonoBehaviour
     [SerializeField] private float moveSpeed = 2.0f;
 
     private Transform currentTarget;
+    // Start is called before the first frame update
     void Start()
     {
         currentTarget = target1;
     }
 
-    void fixedUpdate()
+    // Update is called once per frame
+    void FixedUpdate()
     {
+
         if (transform.position == target1.position)
         {
             currentTarget = target2;
         }
-
         if (transform.position == target2.position)
         {
             currentTarget = target1;
         }
 
         transform.position = Vector2.MoveTowards(transform.position, currentTarget.position, moveSpeed * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player") && other.transform.position.y > transform.position.y)
+        {
+            other.transform.SetParent(transform);
+        }
+
+    }
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            other.transform.SetParent(null);
+        }
+
     }
 }
